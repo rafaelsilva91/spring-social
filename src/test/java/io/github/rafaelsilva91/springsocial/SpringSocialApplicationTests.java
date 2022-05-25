ad6404bb5082a023d2065327657b4750a7b897a9
Rafael Rodrigues <rafaelsilvarodrigues_@hotmail.com>
2022-05-25 12:52:05 -0400

    first commit

---
+++ src/test/java/io/github/rafaelsilva91/springsocial/SpringSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rafaelsilva91.springsocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
